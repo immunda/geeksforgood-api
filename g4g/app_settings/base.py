bda11d3f73da5ed127c2bad90a7663c1511db619
Phil Howell <phil@quae.co.uk>
2014-05-27 22:35:45 +0100

    Added deps, settings and mangement command

---
+++ g4g/app_settings/base.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+class BaseConfig(object):
+
+    DEBUG = False
+    TESTING = False
+
+    SECRET_KEY = ''
---
+++ g4g/app_settings/development.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from g4g.app_settings.base import BaseConfig
+
+
+class Config(BaseConfig):
+
+    DEBUG = True
