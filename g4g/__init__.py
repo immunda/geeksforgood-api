34ced2c8676b6cac0f11a427bdc437eb7e832698
Phil Howell <phil@quae.co.uk>
2014-05-27 22:59:08 +0100

    Add health check endpoint

--- g4g/__init__.py
+++ g4g/__init__.py
@@ -3,3 +3,10 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config.from_object('g4g.settings.Config')
+
+from g4g.views import main
+app.register_blueprint(main)
+
+@app.route("/health-check", methods=['GET'])
+def health_check():
+    return 'OK'
